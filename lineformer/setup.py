8a6c3369709eb0a14c18e0f416992f1268b4f85e
Tdsone <mail@timonschneider.de>
2024-05-31 12:59:53 +0200

    Move setup.py to correct place

--- lineformer/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='lineformer',
-    version='0.1',
-    py_modules=['clean_chart', 'eval', 'infer', 'line_utils', 'lineformer_swin_t_config', 'metric6a'],
-)
